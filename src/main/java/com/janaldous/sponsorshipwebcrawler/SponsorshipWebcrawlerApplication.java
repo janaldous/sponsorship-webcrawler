21a08fdc13e06e30d0ffae4b7678d96414e17c16
Jat Torres <jat.torres@gmail.com>
2021-06-01 00:06:18 +0800

    initial commit

---
+++ src/main/java/com/janaldous/sponsorshipwebcrawler/SponsorshipWebcrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.janaldous.sponsorshipwebcrawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SponsorshipWebcrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SponsorshipWebcrawlerApplication.class, args);
+	}
+
+}
